d768c1159b065a338e5c85683155f1db76335e2b
RealPinkRabbit <sdg0896@gmail.com>
2025-05-08 19:41:09 +0900

    remote-theme -> theme을 사용하도록 롤백

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1746699504',resources: [
+    cacheName: 'chirpy-1746700815',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
